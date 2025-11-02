0c0476c7e3802d3850e4191060848d65c280428e
RickFDND <rick.pijnacker@hva.nl>
2025-11-02 12:59:36 +0100

    counter gemaakt

---
+++ astro/src/components/VueCounter.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <button @click="count--">-</button>
+    <span>{{ count }}</span>
+    <button @click="count++">+</button>
+  </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+    const count = ref(0)
+</script>
